c186a4ca0cd4f778aa5507c9f65d658f5e655568
rianiaprianti <124959390+rianiaprianti@users.noreply.github.com>
2024-02-25 15:07:01 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php
+// definisikan variables
+$nama = 'Ririn';
+$umur = 18;
+$berat = 36;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+ echo "<br/>Hello $nama Apakabar";
+
+ ?>
